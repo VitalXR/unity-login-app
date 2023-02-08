03e45bdaeb111952b730069ff1a29655769b7bfb
“ruha_javed” <ruhajaved2826@gmail.com>
2023-02-07 22:17:41 -0700

    AWS basic auth working with example cognito pool - still need to connect to actual dev cognito pool, figure out why UnAuthorizedException throwing weird exception, and why Debug.Logs not logging as expected in tests. MFA not supported right now.

--- Assets/Script/AwsCognitoAuth.cs
+++ Assets/Script/AwsCognitoAuth.cs
@@ -2,25 +2,78 @@ using UnityEngine;
 using Amazon.CognitoIdentityProvider;
 using Amazon.Extensions.CognitoAuthentication;
 using System.Threading.Tasks;
+using System;
 
-public class AwsCognitoAuth : MonoBehaviour
+public class AwsCognitoAuth
 {
-    private string _poolID = "ca-central-1_9P0HNlZKD";
-    private string _clientID = "e576vvntvnekgrngackrfcc79";
+    string _poolID = "ca-central-1_9P0HNlZKD";
+    string _clientID = "e576vvntvnekgrngackrfcc79";
 
-    public async Task<string> GetTokensAsync()
+    public async Task<Token> GetTokensAsync(string username, string password)
     {
+
         AmazonCognitoIdentityProviderClient provider =
-            new AmazonCognitoIdentityProviderClient(new Amazon.Runtime.AnonymousAWSCredentials());
+            new AmazonCognitoIdentityProviderClient(new Amazon.Runtime.AnonymousAWSCredentials(), Amazon.RegionEndpoint.CACentral1);
         CognitoUserPool userPool = new CognitoUserPool(_poolID, _clientID, provider);
-        CognitoUser user = new CognitoUser("d56aae88-62da-49de-8340-5d7a257c6995", _clientID, userPool, provider);
+        CognitoUser user = new CognitoUser(username, _clientID, userPool, provider);
         InitiateSrpAuthRequest authRequest = new InitiateSrpAuthRequest()
         {
-            Password = "Password123!"
+            Password = password,
         };
 
-        AuthFlowResponse authResponse = await user.StartWithSrpAuthAsync(authRequest).ConfigureAwait(false);
-        string accessToken = authResponse.AuthenticationResult.AccessToken;
-        return accessToken;
+        try
+        {
+            AuthFlowResponse authResponse = await user.StartWithSrpAuthAsync(authRequest).ConfigureAwait(false);
+
+            if (authResponse.AuthenticationResult != null)
+            {
+                Token token = new Token
+                {
+                    accessToken = authResponse.AuthenticationResult.AccessToken,
+                    expiresIn = authResponse.AuthenticationResult.ExpiresIn,
+                    idToken = authResponse.AuthenticationResult.IdToken,
+                    refreshToken = authResponse.AuthenticationResult.RefreshToken,
+                    tokenType = authResponse.AuthenticationResult.TokenType,
+                };
+                return token;
+            }
+            else if (authResponse.ChallengeName == "NEW_PASSWORD_REQUIRED")
+            {
+                throw new AwsCognitoAuthException("User must set new password before logging in.");
+            }
+            else
+            {
+                throw new AwsCognitoAuthException("Login attempt for user unsuccessful.");
+            }
+        }
+        // TODO: Throw some random exception causing issues - exceptioncommon.cs not found. Figure out.
+        //catch (Amazon.CognitoIdentityProvider.Model.NotAuthorizedException e)
+        //{
+        //    throw new AwsCognitoAuthException("Bad username or password.", e);
+        //}
+        catch (Exception e)
+        {
+            throw new AwsCognitoAuthException("something went wrong while authenticating user.", e);
+        }
+    }
+}
+
+public class Token
+{
+    public string accessToken;
+    public int expiresIn;
+    public string idToken;
+    public string refreshToken;
+    public string tokenType;
+}
+
+public class AwsCognitoAuthException : Exception
+{
+    public AwsCognitoAuthException(string message) : base(message)
+    {
+    }
+
+    public AwsCognitoAuthException(string message, Exception inner) : base(message, inner)
+    {
     }
 }
--- Assets/Tests/AwsCognitoAuth_Tests.cs
+++ Assets/Tests/AwsCognitoAuth_Tests.cs
@@ -10,14 +10,17 @@ public class AwsCognitoAuth_Tests
 {
     // A Test behaves as an ordinary method
     [Test]
-    public async void GetAccessTokenTest()
+    public void GetAccessTokenTest()
     {
         // Use the Assert class to test conditions
-        GameObject gameObject = new GameObject("AwsCognitoAuth");
-        AwsCognitoAuth auth = gameObject.AddComponent<AwsCognitoAuth>();
-        var token = await auth.GetTokensAsync();
-        //Console.WriteLine(token);
-        Assert.True(true);
+        AwsCognitoAuth auth = new AwsCognitoAuth();
+        var token = auth.GetTokensAsync("d56aae88-62da-49de-8340-5d7a257c6995", "NewPassword456$");
+        token.Wait();
+        // TODO: Figure out why Debug.Log does not log access token, just says
+        // object, but Console.WriteLine does
+        // Console.WriteLine(token.Result.accessToken);
+        // Debug.Log(token.Result.accessToken);
+        Assert.NotNull(token.Result.accessToken);
     }
 
     //// A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
