b1ab364ccce451a2959a3f5107cb3d6ea8ed5055
“ruha_javed” <ruhajaved2826@gmail.com>
2023-02-15 14:47:22 -0700

    Updated aws auth test file to remove a test that came w/ template.

--- Assets/Tests/AwsCognitoAuth_Tests.cs
+++ Assets/Tests/AwsCognitoAuth_Tests.cs
@@ -18,18 +18,8 @@ public class AwsCognitoAuth_Tests
         token.Wait();
         // TODO: Figure out why Debug.Log does not log access token, just says
         // object, but Console.WriteLine does
-        // Console.WriteLine(token.Result.accessToken);
+        Console.WriteLine(token.Result.accessToken);
         // Debug.Log(token.Result.accessToken);
         Assert.NotNull(token.Result.accessToken);
     }
-
-    //// A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    //// `yield return null;` to skip a frame.
-    //[UnityTest]
-    //public IEnumerator SceneLoader_TestsWithEnumeratorPasses()
-    //{
-    //    // Use the Assert class to test conditions.
-    //    // Use yield to skip a frame.
-    //    yield return null;
-    //}
 }
