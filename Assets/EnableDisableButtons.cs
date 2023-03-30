fc59b9f3d2ba3be878edd0aadd970da7bb11ae11
Hunter-Reboul <hunter_reboul@hotmail.ca>
2023-03-30 00:01:54 -0600

    Changed More Scripts button to Next Page button Added buttons for a second page that are enabled when Next Page is pressed Created a script to jump between the two sets of buttons Added behaviour to Log Out and Exit buttons on Demo Page

---
+++ Assets/EnableDisableButtons.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableDisableButtons : MonoBehaviour
+{
+    public GameObject enabled1;
+    public GameObject enabled2;
+    public GameObject enabled3;
+    public GameObject disabled1;
+    public GameObject disabled2;
+    public GameObject disabled3;
+
+    public void activate()
+    {
+        enabled1.SetActive(false);
+        enabled2.SetActive(false);
+        enabled3.SetActive(false);
+        disabled1.SetActive(true);
+        disabled2.SetActive(true);
+        disabled3.SetActive(true);
+    }
+}
